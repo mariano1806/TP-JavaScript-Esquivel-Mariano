55f30922e6113bfb0acb692afa32d1ad468d7ab7
esquivel mariano <esquivelmariano@18gmail.com>
2024-05-06 09:50:57 -0300

    subi la ultima consigna

---
+++ aseets/diaSemana.js
@@ -0,0 +1,21 @@
+let numero = parseInt(prompt("ingrese un numero del 1 al 7: "));
+let dia = ""; 
+
+if (numero === 1) {
+    dia = "Lunes";       
+} else if (numero === 2) {
+    dia = "Martes";
+} else if (numero === 3) {
+    dia = "Miercoles";
+} else if (numero === 4) {
+    dia = "Jueves";
+} else if (numero === 5) {
+    dia = "Viernes";
+} else if (numero === 6) {
+    dia = "Sabado";
+} else if (numero === 7) {
+    dia = "Domingo";
+} else {
+    dia = "El numero ingresado no es valido";
+}
+alert("El dia de la semana es: " + dia);
