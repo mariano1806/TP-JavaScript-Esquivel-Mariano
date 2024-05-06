72730ae45e4226b77a072f6681376b209bd94b29
esquivel mariano <esquivelmariano@18gmail.com>
2024-05-06 09:36:01 -0300

    agrege dos cambios a numAscendente

--- aseets/numAscendente.js
+++ aseets/numAscendente.js
@@ -2,7 +2,11 @@ let num1 = parseInt(prompt("Ingrese el primer numero: "));
 let num2 = parseInt(prompt("Ingrese el segundo numero: "));
 let num3 = parseInt(prompt("Ingrese el tercer numero: "));
 
-if (num1 <= num2 && num1 <= num3) {
+if (num1 === num2 && num2 === num3) {
+    console.log("Los numeros son iguales");
+} else if (num1 === num2 || num2 === num3 || num1 === num3) {
+    console.log("Dos de los numeros son iguales");
+} else if (num1 <= num2 && num1 <= num3) {
     if (num2 <= num3) {
         console.log ("Los numeros en orden ascendentes son: " + num1 + ", " + num2 + ", " + num3);
     } else {
